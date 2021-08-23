c9ee924debd47cb5243eb59ff261c622492a7b80
muneiah <muneiahtellakula@gmail.com>
2021-08-23 15:11:49 +0530

    doc file

---
+++ Localization/app/src/main/java/com/example/localization/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.localization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}/*telugu sir
+sure sir
+thank you sir*/
